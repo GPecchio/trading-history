a073dc859dc639a7f75b17351d705e388198f83a
Giovanni Pecchio <gp30@rice.edu>
2019-04-03 00:01:45 -0500

    added html template and monthly reports dowload

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, send_from_directory
+app = Flask(__name__, static_url_path='')
+
 
 @app.route("/")
-def hello():
-    return "Hello World!"
+def run():
+    return render_template('index.html')
+
+@app.route('/monthly_reports/<path:path>')
+def send_pdf(path):
+    return send_from_directory('monthly_reports', path)
